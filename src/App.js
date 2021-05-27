c6f62a77534e003b8b8c3b2eafe925b2d3105c6f
Gleb <thetimm8890@gmail.com>
2021-05-27 21:37:53 +0300

    add(NavBar, BrowserRouter, Home, Auto): create basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import {Switch, Route} from 'react-router-dom'
+
+import Home from './views/Home/Home';
+import Auto from './views/Auto/Auto';
+
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" className={classes.root}>
+      <NavBar />
+      <Switch>
+        <Route path={'/cars'} component={Auto}/>
+        <Route path={'/'} exact={true} component={Home}/>
+      </Switch>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Link, withRouter} from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+    menuButton: {
+        marginRight: theme.spacing(2),
+        textDecoration: 'none',
+        color: 'inherit',
+    },
+    title: {
+        textDecoration: 'none',
+        color: 'inherit',
+        flexGrow: 1,
+    },
+}));
+
+function NavBar() {
+    const classes = useStyles();
+  
+    return (
+        <AppBar position="static" color="default">
+            <Toolbar>
+                <Link to="/" className={classes.title}>
+                    <Typography variant="h6">BUYCAR</Typography>
+                </Link>
+                
+                <Link to="/cars" className={classes.menuButton}><Button color="inherit">авто</Button></Link>
+                <Button color="inherit" className={classes.menuButton} variant="outlined">войти</Button>
+                <Button color="inherit" variant="outlined">регистрация</Button>
+            </Toolbar>
+        </AppBar>
+    );
+  }
+  
+  export default NavBar;
